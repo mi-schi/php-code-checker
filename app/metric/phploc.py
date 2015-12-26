cb5cfc736f5e900f4844ea78e7cad1df4cdf3106
Michael Schindler <mis@move-elevator.de>
2015-12-26 14:33:47 +0100

    Fix phploc command

--- app/metric/phploc.py
+++ app/metric/phploc.py
@@ -6,12 +6,16 @@ def execute():
     print '--- phploc ---'
     metric_dir = get_value('metric-dir')
     scan_dir = get_value('project-dir')+get_value('scan-dir')
-    excludes = ' '.join(get_value('exclude-dirs'))
+    exclude_dirs = get_value('exclude-dirs')
+    excludes = ''
+
+    for exclude in exclude_dirs:
+        excludes = excludes+' --exclude '+exclude
 
     print '>>> Metric dir: '+metric_dir
     print '>>> Excludes: '+excludes
 
-    code = php('bin/phploc '+scan_dir+' --count-tests --log-csv '+metric_dir+'phploc.csv --log-xml '+metric_dir+'phploc.xml --exclude '+excludes)
+    code = php('bin/phploc --count-tests --log-csv '+metric_dir+'phploc.csv --log-xml '+metric_dir+'phploc.xml '+excludes+' '+scan_dir)
 
     if code != 0:
         raise SystemExit('There was a error/exception while executing phploc.')
